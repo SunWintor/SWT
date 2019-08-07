366b710e2169221770e2da64d6e697164c9f83f9
SunWintor <793815374@qq.com>
2019-08-07 20:22:56 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,43 @@
+// hello world v2.1.1-release by 冬冬
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"strconv"
+)
+
+// MAGIC GIRL
+const MAGIC = 8579384335739694
+const GIRL = "3bX=<* \\"
+
+func main() {
+	elPsyCongroo(GIRL)
+}
+
+// elPsyCongroo 233333.
+func elPsyCongroo(TAT string) {
+	OAO := []byte(TAT)
+	oAo := ""
+	for _, qwq := range OAO {
+		oAo += strconv.Itoa(int(qwq))
+	}
+	_233, _ := strconv.Atoi(oAo)
+	_2333 := len(fmt.Sprint(MAGIC))
+	_23333, _ := strconv.Atoi(strconv.FormatInt(int64(_2333), 16))
+
+	el := MAGIC ^ _233
+	psy := el / int(math.Pow(float64(_23333), float64(_2333>>1)))
+	rand.Seed(int64(psy))
+	congroo := ""
+	for QAQ := 0; QAQ < _23333; QAQ++ {
+		if QAQ == _23333>>1 {
+			congroo += " "
+			rand.Seed(int64(el%psy + psy))
+		}
+		congroo += string(rand.Int()%'!' + 87)
+	}
+	// el psy congroo
+	fmt.Print(congroo)
+}
