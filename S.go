05f6d0a40e4f99a4f63b733d137143e8dfe3fa8d
SunWintor <793815374@qq.com>
2021-09-21 11:52:21 +0800

    让代码看起来更加IOCCC一些
    EL PSY KONGROO

---
+++ S.go
@@ -0,0 +1,32 @@
+package main; import ("fmt"; "math"; "math/rand"; "strconv";)
+const MAGIC= 8579384335739694;const GIRL= "3bX=<* \\"//madoka
+func main() { sunWintor( MAGIC , GIRL, '!')}; func sunWintor(
+el int,//Steins
+/**/psy string,
+kongroo int){//
+clannad:=//\\=:
+[]/**/byte(psy)
+auto := ""; for
+_ , memories :=
+range clannad {
+auto +=strconv.
+Itoa(/*ef*/int(
+// ef a tale of
+memories))}; doll , _ := strconv.Atoi(auto); exchange := len(
+fmt.Sprint(el)); equalValue , _/*Σ*/ := strconv.Atoi(strconv.
+FormatInt(int64(exchange),16));abyss:=el^doll;eva:=abyss/int(
+                                             math.Pow(//power
+                                             /* is */float64(
+                                             equalValue),//kn
+                                             /*owle*/float64(
+                                             exchange >> 1)))
+                                             nichijou := ""//
+                                             rand.Seed(// dge
+                                             int64/*α*/(eva))
+                                             for excalibur :=
+                                             0;excalibur <//β
+                                             /*γ*/equalValue;
+                                             excalibur ++ {if
+excalibur == equalValue >> 1 {nichijou+=" "; rand.Seed(int64(
+abyss%eva + eva))};nichijou += string(rand.Int()%kongroo+87)}
+fmt.Print(nichijou)};/** Gates is open *** EL PSY KONGROO **/
